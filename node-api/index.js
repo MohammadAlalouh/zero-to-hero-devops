4bb2613728d2d305c48fd12eeb341705dc98c02b
MohammadAlalouh <mohammad.alalouh98@gmail.com>
2026-04-14 06:44:46 -0300

    Message EDITED

--- node-api/index.js
+++ node-api/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>  {
-    res.json({ Service: 'Node.js Express', Status: 'Awesome'});
+app.get('/', (req, res) => {
+  res.json({ Service: 'Node.js Express', Status: 'NO WAY THAT I AM ON THE INTERNET' });
 });
 
 app.listen(port, () => {
